78dca2617dd87ffca8cee007643f04c55e6ac979
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2024-01-24 20:25:15 +0200

    add Tile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { Grid } from "./js/grid";
 
 const gameBoard = document.getElementById("game-board");
-console.log("gameBoard:", gameBoard);
 
 const grid = new Grid(gameBoard);
---
+++ src/js/tile.js
@@ -0,0 +1,11 @@
+class Tile {
+  constructor(gridElement) {
+    this.tileElement = document.createElement("div");
+    this.tileElement.classList.add("tile");
+    this.value = Math.random() > 0.5 ? 2 : 4;
+    this.tileElement.textContent = this.value;
+    gridElement.append(this.tileElement);
+  }
+}
+
+export default Tile;
