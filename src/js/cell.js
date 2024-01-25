4fce1a6ef015391e89f958bd4128a3b8e57ae0ba
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2024-01-25 17:25:56 +0200

    add description comments

--- src/js/cell.js
+++ src/js/cell.js
@@ -1,18 +1,35 @@
 class Cell {
   constructor(gridElement, x, y) {
+    // При создании клетки сразу добавляем ее в дом дерево
     const cell = document.createElement("li");
+    //  + класс для стилизации
     cell.classList.add("cell");
     gridElement.append(cell);
 
+    // Записали в клас координаты которые получим
     this.x = x;
     this.y = y;
   }
 
+  // Метод создает ссылку на ячейку
   linkTile(tile) {
+    // Установим координаты ячейки
     tile.setXY(this.x, this.y);
+    // Запишем в экзепляр нашу ячейку
     this.linkedTile = tile;
+    console.log("this.linkedTile:", this.linkedTile);
+    /**
+     * На выходе имеем 
+     * tileElement: 
+        div.tile
+        value: 2
+        x: 2
+        y: 1
+      */
   }
 
+  // Метод вернет ответ пустая ли ячейка, если значение undefined будет true,
+  //  что есть ответом на вопрос isEmpty или если в linkedTile что то записано, верент false
   isEmpty() {
     return !this.linkedTile;
   }
--- src/js/tile.js
+++ src/js/tile.js
@@ -1,16 +1,24 @@
 class Tile {
   constructor(gridElement) {
+    // Создаем ячейку и добавляем в дом дерево
     this.tileElement = document.createElement("div");
+    // + класс
     this.tileElement.classList.add("tile");
+
+    // Записываем в ячейку рандомное число как в игре, котрое может быть или 2 или 4
     this.value = Math.random() < 0.5 ? 2 : 4;
+
+    // Добавляем текстовый контент ячейке
     this.tileElement.textContent = this.value;
     gridElement.append(this.tileElement);
   }
 
+  // Метод которы установит координаты ячейки (tile)
   setXY(x, y) {
     this.x = x;
     this.y = y;
 
+    // для div.tile будут установлены значения css переменных
     this.tileElement.style.setProperty("--x", x);
     this.tileElement.style.setProperty("--y", y);
   }
